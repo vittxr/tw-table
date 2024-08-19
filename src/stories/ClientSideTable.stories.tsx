d95403d5ee9f529c1d34ce374797ce47d23e5b12
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-19 16:09:59 -0300

    feat: improve mock data.

--- src/stories/ClientSideTable.stories.tsx
+++ src/stories/ClientSideTable.stories.tsx
@@ -2,44 +2,66 @@ import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import Table from "@/Table";
 import { ColumnDef } from "@tanstack/react-table";
+import { mock } from './mock'
 
 type DataType = {
-  id: number;
-  name: string;
-  age: number;
+  user_id: number;
+  username: string;
+  full_name: string;
+  birthdate: string;
+  location: string;
+  bio: string;
+  followers_count: number;
+  profile_pic: string;
+  status: string;
+  last_login: string;
 };
 
 const columns: ColumnDef<DataType>[] = [
   {
-    header: "ID",
-    accessorKey: "id",
+    header: "User ID",
+    accessorKey: "user_id",
   },
   {
-    header: "Name",
-    accessorKey: "name",
+    header: "Username",
+    accessorKey: "username",
   },
   {
-    header: "Age",
-    accessorKey: "age",
+    header: "Full Name",
+    accessorKey: "full_name",
   },
-];
-
-const data: DataType[] = [
-  { id: 1, name: "John Doe", age: 25 },
-  { id: 2, name: "Jane Smith", age: 30 },
-  { id: 3, name: "Alice Johnson", age: 22 },
-  { id: 4, name: "Michael Brown", age: 28 },
-  { id: 5, name: "Emily Davis", age: 27 },
-  { id: 6, name: "Chris Wilson", age: 32 },
-  { id: 7, name: "Olivia Martinez", age: 24 },
-  { id: 8, name: "Daniel Garcia", age: 31 },
-  { id: 9, name: "Sophia Robinson", age: 29 },
-  { id: 10, name: "James Clark", age: 26 },
-  { id: 11, name: "Isabella Walker", age: 23 },
-  { id: 12, name: "Benjamin Hall", age: 33 },
-  { id: 13, name: "Ava King", age: 21 },
-  { id: 14, name: "Lucas Wright", age: 30 },
-  { id: 15, name: "Mia Lewis", age: 28 },
+  {
+    header: "Birthdate",
+    accessorKey: "birthdate",
+  },
+  {
+    header: "Location",
+    accessorKey: "location",
+  },
+  {
+    header: "Bio",
+    accessorKey: "bio",
+    enableSorting: false, // Assuming you don't want to enable sorting on the bio
+  },
+  {
+    header: "Followers Count",
+    accessorKey: "followers_count",
+    enableColumnFilter: false, // Disabling the column filter for followers count
+  },
+  {
+    header: "Profile Picture",
+    accessorKey: "profile_pic",
+    cell: ({ getValue }) => <img src={getValue() as string} alt="Profile" />, // Custom rendering for the image
+    enableSorting: false, // Disabling sorting for profile pictures
+  },
+  {
+    header: "Status",
+    accessorKey: "status",
+  },
+  {
+    header: "Last Login",
+    accessorKey: "last_login",
+  },
 ];
 
 
@@ -54,24 +76,8 @@ export default {
 const Template: StoryFn = (args) => <Table {...args} />;
 
 export const ClientSideTable = Template.bind({});
+
 ClientSideTable.args = {
   columns,
-  data,
-  columnFilters: [],
-  setColumnFilters: () => {},
-  search: {
-    state: {},
-    setState: () => {},
-  },
-  sort: {
-    state: [],
-    setState: () => {},
-  },
-  pagination: {
-    currentPage: 1,
-    rowsCount: 10,
-    pageRange: 5,
-    onPageChange: () => {},
-  },
-  isLoading: false,
+  data: mock,
 };
