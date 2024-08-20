ae25100cc4b2a401f160e8093974bdd49f4d35d6
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-20 16:26:25 -0300

    feat: remove unused svg and add classname for each icon.

--- src/icons/ChevronUp.tsx
+++ src/icons/ChevronUp.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 type Props = {
   className?: string;
 }
-const ChevronUp = (props: Props) => {
+const ChevronUp = ({className}: Props) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
       <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
     </svg>
   )
