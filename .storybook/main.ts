85eba9ea8b0c5b726880ead2e0c33ceb1ead33eb
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-21 15:56:24 -0300

    refactor: change stories path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
